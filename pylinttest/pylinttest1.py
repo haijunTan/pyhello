7a2c7f1b1e0f0df0e5882c43bb75c973258b5725
root <tanhaijun@huawei.com>
2017-07-18 17:53:08 +0800

    add test source file

---
+++ pylinttest/pylinttest1.py
@@ -0,0 +1,14 @@
+
+def unusefunc():
+	print ("unusefunc")
+	unusevary = 1
+
+def funchellworld():
+	print ("funchellworld")
+
+def main():
+	unusevarx = 1
+	funchellworld()
+
+if __name__ == '__main__':
+	main()
